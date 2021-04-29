44281da4ceb14069abd790fe3df98d54443d1669
Everton M. Vieira <everton.muvi@gmail.com>
2021-04-29 18:08:45 -0300

    prototype development

--- pull-all.py
+++ pull-all.py
@@ -1,11 +1,13 @@
 import os
 
 if (__name__ == '__main__'):
-  dirs = os.listdir(".")
-  for file in dirs:
-    if os.path.isdir(file):
-      print("Starting on " + file)
-      os.chdir(file)
-      os.system("git pull")
-      os.chdir("../")
-      print("----------------------------------------------------")
+  confirmar = input("Do you wanna PULL all repositories? (y/N) ")
+  if confirmar == "y":
+      dirs = os.listdir(".")
+      for file in dirs:
+        if os.path.isdir(file):
+          print("Starting on " + file)
+          os.chdir(file)
+          os.system("git pull")
+          os.chdir("../")
+          print("----------------------------------------------------")
--- push-all.py
+++ push-all.py
@@ -1,13 +1,15 @@
 import os
 
 if (__name__ == '__main__'):
-  dirs = os.listdir(".")
-  for file in dirs:
-    if os.path.isdir(file):
-      print("Starting on " + file)
-      os.chdir(file)
-      os.system("git add -A")
-      os.system("git commit -m \"prototype development\"")
-      os.system("git push")
-      os.chdir("../")
-      print("----------------------------------------------------")
+  confirmar = input("Do you wanna PUSH all repositories? (y/N) ")
+  if confirmar == "y":
+    dirs = os.listdir(".")
+    for file in dirs:
+      if os.path.isdir(file):
+        print("Starting on " + file)
+        os.chdir(file)
+        os.system("git add -A")
+        os.system("git commit -m \"prototype development\"")
+        os.system("git push")
+        os.chdir("../")
+        print("----------------------------------------------------")
