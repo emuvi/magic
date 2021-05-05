89953d27f381f74e11527a3d93db71b5468bb3c3
Everton M. Vieira <everton.muvi@gmail.com>
2021-05-05 12:47:19 -0300

    prototype development

--- pull-all.py
+++ pull-all.py
@@ -1,13 +1,15 @@
 import os
 
+# Test
+
 if (__name__ == '__main__'):
   confirmar = input("Do you realy wanna PULL all repositories? (y/N): ")
   if confirmar == "y":
-      dirs = os.listdir(".")
-      for file in dirs:
-        if os.path.isdir(file):
-          print("Starting on " + file)
-          os.chdir(file)
-          os.system("git pull")
-          os.chdir("../")
-          print("----------------------------------------------------")
+    dirs = os.listdir(".")
+    for file in dirs:
+      if os.path.isdir(file):
+        print("Starting on " + file)
+        os.chdir(file)
+        os.system("git pull")
+        os.chdir("../")
+        print("----------------------------------------------------")
