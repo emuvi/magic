2be4ec09b4082af454d7524684a6aab2e25b18a3
Éverton M. Vieira <everton.muvi@gmail.com>
2021-08-14 11:01:59 -0300

    prototype development

--- push-all.py
+++ push-all.py
@@ -3,6 +3,7 @@ import os
 if (__name__ == '__main__'):
   confirm = input("Do you really wanna PUSH all repositories? (y/N): ")
   if confirm == "y":
+    os.chdir("..")
     dirs = os.listdir(".")
     for file in dirs:
       if os.path.isdir(file):
