5f78974910ce35d198d7b2a9ffc4b5dd036fe7ff
Éverton M. Vieira <everton.muvi@gmail.com>
2021-08-16 09:47:05 -0300

    prototype development

--- pull-all.py
+++ pull-all.py
@@ -1,14 +1,14 @@
 import os
 
 if (__name__ == '__main__'):
-  confirm = input("Do you really wanna PULL all repositories? (y/N): ")
-  if confirm == "y":
-    os.chdir("..")
-    dirs = os.listdir(".")
-    for file in dirs:
-      if os.path.isdir(file):
-        print("Starting on " + file)
-        os.chdir(file)
-        os.system("git pull")
-        os.chdir("../")
-        print("----------------------------------------------------")
+    confirm = input("Do you really wanna PULL all repositories? (y/N): ")
+    if confirm == "y":
+        os.chdir("..")
+        dirs = os.listdir(".")
+        for file in dirs:
+            if os.path.isdir(file):
+                print("Starting on " + file)
+                os.chdir(file)
+                os.system("git pull")
+                os.chdir("../")
+                print("----------------------------------------------------")
--- push-all.py
+++ push-all.py
@@ -1,16 +1,16 @@
 import os
 
 if (__name__ == '__main__'):
-  confirm = input("Do you really wanna PUSH all repositories? (y/N): ")
-  if confirm == "y":
-    os.chdir("..")
-    dirs = os.listdir(".")
-    for file in dirs:
-      if os.path.isdir(file):
-        print("Starting on " + file)
-        os.chdir(file)
-        os.system("git add -A")
-        os.system("git commit -m \"prototype development\"")
-        os.system("git push")
-        os.chdir("../")
-        print("----------------------------------------------------")
+    confirm = input("Do you really wanna PUSH all repositories? (y/N): ")
+    if confirm == "y":
+        os.chdir("..")
+        dirs = os.listdir(".")
+        for file in dirs:
+            if os.path.isdir(file):
+                print("Starting on " + file)
+                os.chdir(file)
+                os.system("git add -A")
+                os.system("git commit -m \"prototype development\"")
+                os.system("git push")
+                os.chdir("../")
+                print("----------------------------------------------------")
