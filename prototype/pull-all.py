226bd1b7c3e7a7c345deb47884524fd48146069b
Éverton M. Vieira <everton.muvi@gmail.com>
2021-09-02 11:52:31 -0300

    prototype development

--- prototype/pull-all.py
+++ prototype/pull-all.py
@@ -1,15 +1,33 @@
 import os
+import subprocess
+from threading import Thread
+
+
+class Runner(Thread):
+    def __init__(self, name, path):
+        Thread.__init__(self)
+        self.name = name
+        self.path = path
+
+    def run(self):
+        print("Starting on " + self.name)
+        with subprocess.Popen(["git", "pull"], cwd=self.path, stdout=subprocess.PIPE) as proc:
+            print("From " + self.name + ": " + str(proc.stdout.read().decode("utf-8")))
+
 
 if (__name__ == '__main__'):
     confirm = input("Do you really wanna PULL all repositories? (y/N): ")
     if confirm == "y":
         os.chdir("..")
         os.chdir("..")
+        root = os.getcwd()
         dirs = os.listdir(".")
-        for file in dirs:
-            if os.path.isdir(file):
-                print("Starting on " + file)
-                os.chdir(file)
-                os.system("git pull")
-                os.chdir("../")
-                print("----------------------------------------------------")
+        threads = []
+        for inside in dirs:
+            path = os.path.join(root, inside)
+            if os.path.isdir(path):
+                thread = Runner(inside, path)
+                thread.start()
+                threads.append(thread)
+        for thread in threads:
+            thread.join()
--- prototype/push-all.py
+++ prototype/push-all.py
@@ -1,17 +1,37 @@
 import os
+import subprocess
+from threading import Thread
+
+
+class Runner(Thread):
+    def __init__(self, name, path):
+        Thread.__init__(self)
+        self.name = name
+        self.path = path
+
+    def run(self):
+        print("Starting on " + self.name)
+        with subprocess.Popen(["git", "add", "-A"], cwd=self.path, stdout=subprocess.PIPE) as proc:
+            print("From " + self.name + ": " + str(proc.stdout.read().decode("utf-8")))
+        with subprocess.Popen(["git", "commit", "-m", "prototype development"], cwd=self.path, stdout=subprocess.PIPE) as proc:
+            print("From " + self.name + ": " + str(proc.stdout.read().decode("utf-8")))
+        with subprocess.Popen(["git", "push"], cwd=self.path, stdout=subprocess.PIPE) as proc:
+            print("From " + self.name + ": " + str(proc.stdout.read().decode("utf-8")))
+
 
 if (__name__ == '__main__'):
     confirm = input("Do you really wanna PUSH all repositories? (y/N): ")
     if confirm == "y":
         os.chdir("..")
         os.chdir("..")
+        root = os.getcwd()
         dirs = os.listdir(".")
-        for file in dirs:
-            if os.path.isdir(file):
-                print("Starting on " + file)
-                os.chdir(file)
-                os.system("git add -A")
-                os.system("git commit -m \"prototype development\"")
-                os.system("git push")
-                os.chdir("../")
-                print("----------------------------------------------------")
+        threads = []
+        for inside in dirs:
+            path = os.path.join(root, inside)
+            if os.path.isdir(path):
+                thread = Runner(inside, path)
+                thread.start()
+                threads.append(thread)
+        for thread in threads:
+            thread.join()
