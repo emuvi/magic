b87394926bf2bff4840f28a79420b4589f0972f9
Éverton M. Vieira <everton.muvi@gmail.com>
2021-09-10 10:30:05 -0300

    prototype development

--- vscode/settings-get.bat
+++ vscode/settings-get.bat
@@ -1,4 +1,3 @@
-@echo off
 setlocal
 :PROMPT
 SET /P AREYOUSURE=Are you sure (y/[N])?
@@ -6,5 +5,6 @@ IF /I "%AREYOUSURE%" NEQ "y" GOTO END
 rm C:\Code\magic\vscode\settings-old.json
 mv C:\Code\magic\vscode\settings.json C:\Code\magic\vscode\settings-old.json
 cp %UserProfile%\AppData\Roaming\Code\User\settings.json C:\Code\magic\vscode\settings.json
+pause
 :END
 endlocal
