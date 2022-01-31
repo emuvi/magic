21bfd48d0b3c193c8cfbb4997e7d942ccc57ed87
Éverton M. Vieira <everton.muvi@gmail.com>
2022-01-31 20:44:13 +0000

    prototype

--- docker/poidel/fish/config.fish
+++ docker/poidel/fish/config.fish
@@ -1,3 +1,6 @@
 set HELLO fishing
 
 alias tmux='tmux -u'
+tmux bind c new-window -c "#{pane_current_path}"
+tmux bind '"' split-window -c "#{pane_current_path}"
+tmux bind % split-window -h -c "#{pane_current_path}"
