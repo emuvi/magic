aa9ffcc7aaae68effc9ed3a44034612cbaa8910b
Éverton M. Vieira <everton.muvi@gmail.com>
2022-02-01 15:48:39 +0000

    prototype

--- docker/pinba/enter.bat
+++ docker/pinba/enter.bat
@@ -1,2 +1,2 @@
 @echo off
-docker run --rm -it -v C:\Devs:/Devs pinba /bin/bash
+docker run --rm -it -v C:\Devs:/Devs -p 5490:5490 pinba /bin/bash
---
+++ docker/pinde/start.bat
@@ -0,0 +1,2 @@
+@echo off
+docker run --rm -td -v C:\Devs:/Devs -v C:\Devs\Home\.vscode-server:/root/.vscode-server pinde /usr/bin/fish
